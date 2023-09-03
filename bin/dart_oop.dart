5b65d4debadc8bbbb81688b4c574be36db16dbb8
Pedro Henrique <pedrohbc96@gmail.com>
2023-09-03 20:51:32 -0300

    feat: adiciona get e set

--- bin/dart_oop.dart
+++ bin/dart_oop.dart
@@ -2,4 +2,9 @@ import 'package:dart_oop/classes/Pessoa.dart';
 
 void main(List<String> arguments) {
   var p1 = new Pessoa(); // new não é necessário mas é didático
+  p1.setNome("João");
+  p1.setEndereco("Rua 1");
+
+  print(p1.getNome());
+  print(p1.getEndereco());
 }
--- lib/classes/Pessoa.dart
+++ lib/classes/Pessoa.dart
@@ -1,1 +1,21 @@
-class Pessoa{}
+class Pessoa{
+  String _nome = ""; // nullable safety mas pode ser => String? nome;
+  String _endereco = ""; // underline antes do nome para encapsular como privado
+
+  String getNome() {
+    return _nome;
+  }
+
+  void setNome(String nome) {
+    _nome = nome;
+  }
+
+  String getEndereco() {
+    return _endereco;
+  }
+
+  void setEndereco(String endereco) {
+    _endereco = endereco;
+  }
+}
+
