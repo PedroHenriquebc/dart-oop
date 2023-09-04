8077b479bcd58bc48b646f0ecb08676a1d8c5f5f
Pedro Henrique <pedrohbc96@gmail.com>
2023-09-04 10:26:13 -0300

    feat: adiciona classes abstratas

--- lib/classes/Pessoa.dart
+++ lib/classes/Pessoa.dart
@@ -1,4 +1,4 @@
-class Pessoa{
+abstract class Pessoa{
   String _nome = ""; // nullable safety mas pode ser => String? nome; (para poder receber null)
   String _endereco = ""; // underline antes do nome para encapsular como privado
 
